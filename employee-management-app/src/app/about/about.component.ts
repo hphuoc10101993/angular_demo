b39517dfcc4239214cdbed75684dd8f18c5c4245
vacation_book <hphuoc10101993@gmail.com>
2019-02-22 18:00:06 +0700

    Implement emit event for customize event

--- employee-management-app/src/app/about/about.component.ts
+++ employee-management-app/src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  /* demo about emit */
+  visible = true;
+  @Output() open: EventEmitter<any> = new EventEmitter<any>();
+  @Output() close: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggle() {
+      this.visible = !this.visible;
+      if (this.visible) {
+          this.open.emit(null);
+      } else {
+          this.close.emit(null);
+      }
   }
 
 }
